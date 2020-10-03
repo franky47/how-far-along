cc1407fdcce2c60fc16ff62908b839ee1ce4931b
Francois Best <francois@francoisbest.com>
2020-10-03 18:07:01 +0200

    chore: Remove Sentry

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,3 @@
 import { createChakraNextApp } from '@47ng/chakra-next'
-import { SentryErrorBoundary } from 'src/services/sentry'
 
-export default createChakraNextApp({
-  Providers: ({ children }) => (
-    <SentryErrorBoundary>{children}</SentryErrorBoundary>
-  )
-})
+export default createChakraNextApp()
